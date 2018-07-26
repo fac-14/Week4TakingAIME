670fa6b1eb3fe75c4cb1ef1266d2aa1b6fdafc41
EmilyScripts <emily.rever@gmail.com>
2018-07-26 10:54:03 +0100

    adds event listner to field, store text content

--- public/logic.js
+++ public/logic.js
@@ -1,6 +1,12 @@
-(function() {
+// (function() {
   const form = document.getElementById("form");
   const submitButton = document.getElementById("submit-button");
   const searchBox = document.getElementById("input");
   const resultDiv = document.getElementById("result");
-})();
+  // console.log(searchBox);
+  var textInput = '';
+  searchBox.addEventListener('input', function(event) {
+    textInput += event.data;
+    resultDiv.textContent = textInput;
+  })
+// })();
