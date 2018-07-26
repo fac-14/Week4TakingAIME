78e2a40729372d970cb8970cd166bd828b09ee67
sima-qian <29804933+sima-qian@users.noreply.github.com>
2018-07-26 10:32:34 +0100

    a few small changes

--- src/router.js
+++ src/router.js
@@ -7,9 +7,9 @@ const router = (request, response) => {
     handleHomeRoute(request, response, url);
   } else if (url.indexOf("/public/") !== -1) {
     handlePublic(request, response, url);
-  } else if (url.indexOf("?q=") !== -1) {
-    response.writeHead(404, "Content-Type: text/html");
-    response.end("<h1>Doing the right stuff</h1>");
+    // } else if (url.indexOf("?q=") !== -1) {
+    //   response.writeHead(404, "Content-Type: text/html");
+    //   response.end("<h1>Doing the right stuff</h1>");
   } else {
     response.writeHead(404, "Content-Type: text/html");
     response.end("<h1>404 Not Found - Beached As! 🐳 </h1>");
